fca056638186765e9242a5972e3a94ce57a86f1d
Willie Payne <williepain@gmail.com>
2016-02-09 16:35:46 -0700

    added food fields and changed duration to be a bit longer for providers

--- workers/uber/simulate-providers.js
+++ workers/uber/simulate-providers.js
@@ -12,6 +12,11 @@ var city_location = {
   lon: -122.41
 }
 
+var specialties = ['American', 'Mexican', 'Chinese', 'Japanese', 'Indian', 'Breakfast', 'Coctails', 'Dessert'];
+var ratings = [1, 2, 2, 3, 3, 3, 4, 4, 5];
+var types = ['Standard', 'Premium']
+
+
 var radius = 0.03
 
 // simualate a random person entering, staying for a duration, and leaving
@@ -19,15 +24,24 @@ function simulate(){
 
   // generate a random person with a random name,
   // random location, and random duration
-  var name = random_name()
-  var duration = 1 + 5 * Math.random()
-  var lat = city_location.lat + radius * (Math.random() - 0.5) * 2
-  var lon = city_location.lon + radius * (Math.random() - 0.5) * 2
+  var name = random_name();
+  var duration = 1 + 15 * Math.random();
+  var lat = city_location.lat + radius * (Math.random() - 0.5) * 2;
+  var lon = city_location.lon + radius * (Math.random() - 0.5) * 2;
+  var price = Math.floor(Math.random() * (1000 - 20 + 1)) + 20; // Int ranges between 20 and 1000
+  var rating = ratings[Math.floor(Math.random() * (8 - 0 + 1))];
+  var specialty = specialties[Math.floor(Math.random() * (8 - 0 + 1))];
+  var type = types[Math.floor(Math.random() * (1 - 0 + 1))];
+  
   var person = {
     name: name,
     duration: duration,
     lat: lat,
-    lon: lon
+    lon: lon,
+    price: price,
+    rating: rating,
+    specialty: specialty,
+    type: type
   }
 
   // simulate this person entering
@@ -48,7 +62,11 @@ function enter(person){
   personRef.set({
     lat: person.lat,
     lon: person.lon,
-    name: person.name
+    name: person.name,
+    price: person.price,
+    rating: person.rating,
+    specialty: person.specialty,
+    type: person.type
   });
 }
 
